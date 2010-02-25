764ad1d9e6424360b3c7e566f5b682a1e8f23bcf
Russell Jones <spam@codeofficer.com>
2010-02-25 17:13:58 -0500

    meep

--- experiments/hello_world/helloWorld.js
+++ experiments/hello_world/helloWorld.js
@@ -6,10 +6,19 @@
 // 
 	var sys = require("sys");
 	var http = require("http");
+	
 	http.createServer(function (request, response) {
-		response.writeHeader(200, {"Content-Type": "text/plain"});
-		response.write("Hello World\n");
-		response.close();
-		log.debug('****************** request served');
+		// response.writeHeader(200, {"Content-Type": "text/plain"});
+		// response.write("Hello World\n");
+		// response.close();
+		// log.debug('ping!');
+		// log.inspect(request);
+	  setTimeout(function() {
+	    response.writeHeader(200, {'Content-Type': 'text/html'});
+	    response.write('<h1>Hello World</h1>');
+	    response.close();
+			log.debug('ping!');
+	  }, 1000);
 	}).listen(8000);
+
 	log.info('Server running at http://127.0.0.1:8000/');
